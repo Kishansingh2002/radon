45d9a22b75de15fc4ce1fb05a5dbe42f0f55a685
Kishansingh2002 <ks8416921522@gmail.com>
2022-06-01 18:14:24 +0530

    nodejs

--- src/routes/logger.js
+++ src/routes/logger.js
@@ -1,10 +1,10 @@
-const endpoint = "https://www.functionup.org"
+
 const batch = "radon"
 
-const log = function() {
+const welcome  = function() {
     console.log('I am inside the log function')
 }
 
-module.exports.endpoint = endpoint
+//module.exports.endpoint = endpoint
 module.exports.batch = batch
-module.exports.log = log
+module.exports.welcome = welcome
--- src/routes/route.js
+++ src/routes/route.js
@@ -4,26 +4,26 @@ const externalModule = require('./logger')
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
-    console.log('The constant in logger route has a value '+externalModule.endpoint)
+    
     console.log('The current batch is '+externalModule.batch)
-    externalModule.log()
-    res.send('My first ever api!')
+    externalModule.welcome()
+    res.send('welcome to my application. i am kishan singh and a part of Function Up Redon Cohort')
 });
 
 router.get('/test-me1', function (req, res) {
-    res.send('My second ever api!')
+    res.send('My second welcome to my application. i am kishan singh and a part of Function Up Redon Cohort ')
 });
 
 router.get('/test-me2', function (req, res) {
-    res.send('My third api!')
+    res.send('My 2nd line welcome to my application. i am kishan singh and a part of Function UpREdon Cohort')
 });
 
 router.get('/test-me3', function (req, res) {
-    res.send('My 4th api!')
+    res.send('My 4th welcome to my application. i am kishan singh and a part of Function UpREdon Cohort')
 });
 
 router.get('/test-me4', function (req, res) {
-    res.send('My last api!')
+    res.send('My last welcome to my application. i am kishan singh and a part of Function UpREdon Cohortnode')
 });
 
 module.exports = router;
