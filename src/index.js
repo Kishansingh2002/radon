f15e17ac85a0d1bb5dcd548d7e574cc00e8a7d70
Kishansingh2002 <ks8416921522@gmail.com>
2022-06-05 09:55:32 +0530

    demo

--- src/index.js
+++ src/index.js
@@ -8,9 +8,16 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
+const mongoose = require('mongoose')
+mongoose.connect("mongodb+srv://kishan_31:4GdZARnCyGUbPKKw@cluster0.d5f50qs.mongodb.net/?retryWrites=true&w=majorityv", { userNewUrlParser: ture })
+  .then(() => console.log('mongodb running on 27017'))
+  .catch(err => console.log(err))
+
+
 app.use('/', route);
 
-app.listen(process.env.PORT || 3001, function() {
-    console.log('Express app running on port ' + (process.env.PORT || 3001))
+app.listen(process.env.PORT || 5001, function () {
+  console.log('Express app running on port ' + (process.env.PORT || 5001))
 });
 
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,53 +1,15 @@
 const express = require('express');
-const res = require('express/lib/response');
-//const myHelper = require('../util/helper')
-//const underscore = require('underscore')
-
 const router = express.Router();
+const useModel= require("./userModel");
 
-router.get('/hello', function (req, res) {
-    let _ = require("lodash");
-    let arr = ['jan', 'feb', 'march', 'april', 'may', 'june', 'july', 'aug', 'sep', 'oct', 'nov', 'dec'];
-    console.log("Before: ", arr)
-    
-
-    // Making chunks of size 3
-    console.log("After: ", _.chunk(arr, 3))
-    
-  
+const UserController=require("../routes/userController");
 
 
-
-    const arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
-
-
-    const odds = arr1.filter(number => {
-        return number % 2 !== 0;
-    });
-
-    console.log(_.tail(odds));
-
-
-
-
-
-
-//const _ = require("lodash");  
-
-// Use of _.union() method 
-    let gfg = _.union([20, 12, 6, 15], [8, 15, 6], [6, 8, 9], [9, 8, 5], [9, 6, 7]);
-// Printing the output  
-    console.log(gfg)
-// const _ = require('lodash');
-
-    let pairs = [['x', 1], ['y', 2], ['z', 3]]
-
-    let obj = _.fromPairs(pairs);
-
-    console.log(obj)
-});
-
+router.post('/CreateUser', UserController.creater);
+router.get('/getAllUsers', UserController.getUserData);
 
 module.exports = router;
-// adding this comment for no reason
+
+
+
 
