cb7b4976962f13724638544e3270ad6a301e2525
Kishansingh2002 <ks8416921522@gmail.com>
2022-07-26 09:58:25 +0530

    project5

--- src/Controllers/urlController.js
+++ src/Controllers/urlController.js
@@ -1,5 +1,30 @@
 const validUrl = require("valid-url");
 const shortid = require("shortid");
+const redis = require("redis");
+
+const { promisify } = require("util");
+
+//Connect to redis
+const redisClient = redis.createClient(
+  13190,
+  "redis-13190.c301.ap-south-1-1.ec2.cloud.redislabs.com",
+  { no_ready_check: true }
+);
+redisClient.auth("gkiOIPkytPI3ADi14jHMSWkZEo2J5TDG", function (err) {
+  if (err) throw err;
+});
+
+redisClient.on("connect", async function () {
+  console.log("Connected to Redis..");
+});
+
+//1. connect to the server
+//2. use the commands :
+
+//Connection setup for redis
+
+const SET_ASYNC = promisify(redisClient.SET).bind(redisClient);
+const GET_ASYNC = promisify(redisClient.GET).bind(redisClient);
 
 const UrlModel = require("../models/urlModel");
 const isValid = function (value) {
@@ -17,10 +42,6 @@ const createUrl = async (req, res) => {
         status: false,
         message: "Invalid URL Please Enter valid details",
       });
-    // if (!longUrl)
-    //   return res
-    //     .status(400)
-    //     .send({ status: false, message: "longUrl is required" });
 
     if (!isValid(longUrl)) {
       return res.status(400).send({ status: false, msg: "please enter url" });
@@ -29,6 +50,13 @@ const createUrl = async (req, res) => {
     // Check base url
     if (!validUrl.isUri(baseUrl)) {
       return res.status(401).json("Invalid base url");
+    }
+    let getUrl = await GET_ASYNC(`${longUrl}`);
+    let fetchurl = JSON.parse(getUrl); //string se hatane ka kam krta hai
+    if (fetchurl) {
+      return res
+        .status(200)
+        .send({ status: true, message: " fetch Successfull", data: fetchurl });
     }
 
     // Create url code
@@ -57,6 +85,7 @@ const createUrl = async (req, res) => {
       createdAt: 0,
       updatedAt: 0,
     });
+    await SET_ASYNC(`${longUrl}`, JSON.stringify(mainData));
 
     return res.status(201).send({
       status: true,
@@ -71,12 +100,19 @@ const createUrl = async (req, res) => {
 const getUrl = async (req, res) => {
   try {
     let urlCode = req.params.urlCode;
+    let cachedLongUrl = await GET_ASYNC(`${urlCode}`);
+    if (cachedLongUrl) {
+      console.log("redirect from cache");
+      return res.status(302).redirect(JSON.parse(cachedLongUrl));
+    }
     const checkurl = await UrlModel.findOne({ urlCode: urlCode }); //hm jo urlCode dal rhe hai vo DB me hai ki nhi
 
     if (!checkurl) {
+      await SET_ASYNC(`${urlCode}`, JSON.stringify(url.longUrl));
+      console.log("redirect from DB");
       return res.status(302).redirect(checkurl.longUrl); //check url mil gaya to redirect kr dege
     }
-    return res.status(404).send({ status: false, msg: "No url found" }); //checkurl nhi milega to ye msg aayega
+    return res.status(400).send({ status: false, msg: "No url found" }); //checkurl nhi milega to ye msg aayega
   } catch (err) {
     console.error(err);
     res.status(500).send({ status: false, msg: err.msg });
